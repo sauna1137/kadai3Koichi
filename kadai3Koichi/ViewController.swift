67bc6e6ccbc23bacc466065bf0c720394b0183d1
koichi <soneko2102@gmail.com>
2021-08-21 08:27:22 +0900

    Initial Commit

---
+++ kadai3Koichi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kadai3Koichi
+//
+//  Created by KS on 2021/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
